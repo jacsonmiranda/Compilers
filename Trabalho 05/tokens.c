731b97179ef222bb99d667e7e1773462c39e1e1e
Jacson Miranda <jacsonmiranda@gmail.com>
2020-09-12 22:44:52 -0300

    activity 5 added

---
+++ Trabalho 05/tokens.c
@@ -0,0 +1,31 @@
+#ifndef _TOKENS_H_
+#define _TOKENS_H_
+ 
+#define KW_INT        256
+#define KW_REAL       257
+#define KW_BOOL       258
+#define KW_CHAR       259
+#define KW_IF         261
+#define KW_ELSE       263
+#define KW_LOOP       264
+#define KW_INPUT      266
+#define KW_RETURN     267
+#define KW_OUTPUT     268
+
+#define OP_LE         270
+#define OP_GE         271
+#define OP_EQ         272
+#define OP_NE         273
+#define OP_AND        274
+#define OP_OR         275
+
+#define TK_IDENTIFIER 280
+#define LIT_INTEGER   281
+#define LIT_FALSE     283
+#define LIT_TRUE      284
+#define LIT_CHAR      285
+#define LIT_STRING    286
+
+#define TOKEN_ERROR   290
+
+#endif /* _TOKENS_H_ */
